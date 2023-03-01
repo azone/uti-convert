f157d3aa808dfcb232f47e65ecab82f92a15e404
Yozone Wang <wangyaozh@gmail.com>
2023-03-01 10:51:29 +0800

    fix warnings

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .executableTarget(
             name: "uti-convert",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
